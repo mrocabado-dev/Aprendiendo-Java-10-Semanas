39ce67a029956bc0991e3110f67e735d6345fa0e
mrocabado-dev <mrocabadovargas@icloud.com>
2026-05-28 22:52:15 -0400

    feat: mostrar tarjetas de contactos reutilizables

--- semana-08-agenda-web/src/main/java/com/organizacion/agenda/views/ContactosView.java
+++ semana-08-agenda-web/src/main/java/com/organizacion/agenda/views/ContactosView.java
@@ -1,20 +1,27 @@
-package com.organizacion.agenda.views;
+package com.organizacion.app.views;
+
+import com.organizacion.app.ui.MainLayout;
+import com.organizacion.app.ui.TarjetaContacto;
 
 import com.vaadin.flow.component.button.Button;
 
-import com.vaadin.flow.component.html.H1;
-
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.H2;
 
 import com.vaadin.flow.component.notification
         .Notification;
+
+import com.vaadin.flow.component.orderedlayout
+        .HorizontalLayout;
 
 import com.vaadin.flow.component.orderedlayout
         .VerticalLayout;
 
 import com.vaadin.flow.router.Route;
 
-@Route("contactos")
+@Route(
+        value = "contactos",
+        layout = MainLayout.class
+)
 
 public class ContactosView
         extends VerticalLayout {
@@ -25,41 +32,59 @@ public class ContactosView
 
         setSpacing(true);
 
-        H1 titulo =
-                new H1(
-                        "Contactos"
+        H2 titulo =
+                new H2(
+                        "Lista de Contactos"
                 );
 
-        Paragraph mensaje =
-                new Paragraph(
-                        "Aqui apareceran "
-                                + "los contactos. "
-                                + "Proxima semana."
-                );
-
-        Button volver =
+        Button agregar =
                 new Button(
-                        "Volver al inicio"
+                        "Agregar contacto"
                 );
 
-        volver.addClickListener(
-                e ->
+        agregar.addClickListener(
 
-                        getUI().ifPresent(
-
-                                ui ->
-                                        ui.navigate("")
-                        )
+                e -> Notification.show(
+                        "Funcion agregar pendiente"
+                )
         );
 
-        Notification.show(
-                "Vista de contactos cargada"
+        HorizontalLayout contenedor =
+                new HorizontalLayout();
+
+        contenedor.setSpacing(true);
+
+        TarjetaContacto c1 =
+                new TarjetaContacto(
+                        "Ana Lopez",
+                        "76543210",
+                        "Amigos"
+                );
+
+        TarjetaContacto c2 =
+                new TarjetaContacto(
+                        "Carlos Rios",
+                        "71112233",
+                        "Trabajo"
+                );
+
+        TarjetaContacto c3 =
+                new TarjetaContacto(
+                        "Maria Vega",
+                        "72223344",
+                        "Familia"
+                );
+
+        contenedor.add(
+                c1,
+                c2,
+                c3
         );
 
         add(
                 titulo,
-                mensaje,
-                volver
+                agregar,
+                contenedor
         );
     }
 }
